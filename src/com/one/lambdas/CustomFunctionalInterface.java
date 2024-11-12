aa9142a13adc640d9d08a260eff56e661819f3f8
Kasun Buddhi Ranaweera <kasunbuddhi@gmail.com>
2024-11-12 08:40:32 +0530

    initiate project

---
+++ src/com/one/lambdas/CustomFunctionalInterface.java
@@ -0,0 +1,4 @@
+package com.one.lambdas;
+
+public class CustomFunctionalInterface {
+}
---
+++ src/com/one/lambdas/FirstLambda.java
@@ -0,0 +1,29 @@
+package com.one.lambdas;
+
+public class FirstLambda {
+    public static void main(String[] args) {
+        //pre-java 8
+        I i = new I(){
+            @Override
+            public void m(){
+                System.out.println("I::m()");
+            }
+        };
+        i.m();
+
+        //Java 8 - Lambda expression (method code)
+        I lambdaI = () -> {
+            System.out.println("Lambda version (method code)");
+        };
+
+        I lambda2 = () ->  System.out.println("Lambda version");
+
+        lambdaI.m();
+        lambda2.m();
+    }
+}
+
+//functional interface, it has only one abstract method
+interface I{
+    void m();
+}
