4a433ed14e1652df7b9e926e6dcf8e6864253bfc
Kasun Buddhi Ranaweera <kasunbuddhi@gmail.com>
2024-11-12 09:06:06 +0530

    initiate project

--- src/com/one/lambdas/CustomFunctionalInterface.java
+++ src/com/one/lambdas/CustomFunctionalInterface.java
@@ -1,4 +1,24 @@
 package com.one.lambdas;
 
+import java.util.function.Predicate;
+
 public class CustomFunctionalInterface {
+    public static void main(String[] args) {
+        Evaluate<Integer> lambda = i -> i < 0;
+        System.out.println("Evaluate: " + lambda.isNavigate(-1));
+        System.out.println("Evaluate: " + lambda.isNavigate(+1));
+
+        Predicate<Integer> predicate = i -> i < 0;
+        System.out.println("Predicate: " + predicate.test(-1));
+        System.out.println("Predicate: " + predicate.test(+1));
+
+        Evaluate<Integer> lambda2 = i -> { return i < 0;};
+        System.out.println("Evaluate: " + lambda2.isNavigate(-1));
+        System.out.println("Evaluate: " + lambda2.isNavigate(+1));
+    }
+
+}
+
+interface Evaluate<T> {
+    boolean isNavigate(T t);
 }
