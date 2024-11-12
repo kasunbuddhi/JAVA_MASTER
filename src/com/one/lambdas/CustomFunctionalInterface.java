51b717ce2b0d46d55ed0a74967263a5a6ddea10f
Kasun Buddhi Ranaweera <kasunbuddhi@gmail.com>
2024-11-12 22:19:17 +0530

    using predicate

--- src/com/one/lambdas/CustomFunctionalInterface.java
+++ src/com/one/lambdas/CustomFunctionalInterface.java
@@ -15,8 +15,26 @@ public class CustomFunctionalInterface {
         Evaluate<Integer> lambda2 = i -> { return i < 0;};
         System.out.println("Evaluate: " + lambda2.isNavigate(-1));
         System.out.println("Evaluate: " + lambda2.isNavigate(+1));
+
+        int x = 4;
+        System.out.println("Is " + x + "even?"+ check(x, n -> n%2 == 0));
+        x = 7;
+        System.out.println("Is " + x + "even?"+ check(x, n -> n%2 == 0));
+
+        String name = "Mr. Virat Kohli";
+        System.out.println("Does " + name + "Starts with Mr.?"+ check(name, n -> n.startsWith("Mr.")));
+
+        name = "Ms. Anushka Sharma";
+        System.out.println("Does " + name + "Starts with Mr.?"+ check(name, n -> n.startsWith("Mr.")));
+    }
+
+    public static <T> boolean check(T t, Predicate<T> lambda) {
+        return lambda.test(t);
+
     }
 }
+
+
 
 interface Evaluate<T> {
     boolean isNavigate(T t);
