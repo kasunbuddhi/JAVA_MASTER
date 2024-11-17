297093154d92a8d04544a0a91493983a47c6a443
Kasun Buddhi Ranaweera <kasunbuddhi@gmail.com>
2024-11-17 23:25:15 +0530

    Streams Intermediate Operators

--- src/com/one/stream/IntermediateOperationsPractices.java
+++ src/com/one/stream/IntermediateOperationsPractices.java
@@ -15,5 +15,18 @@ public class IntermediateOperationsPractices {
                 .peek(s -> System.out.println("1."+s))
                 .distinct()
                 .forEach(s -> System.out.println("2."+s));
+
+        //limit
+        Stream.of(11, 22, 33, 44, 10, 20, 55, 30, 66, 77, 88, 99 )
+                .peek(n -> System.out.print(" A - " + n))
+                .filter(n -> n > 40)
+                .peek(n -> System.out.print(" B - " + n))
+                .limit(2)
+                .forEach(n -> System.out.println(" C - " + n));
+
+        //map
+        Stream.of("book", "pen", "ruler")
+                .map(s -> s.length())
+                .forEach(System.out::print);
     }
 }
