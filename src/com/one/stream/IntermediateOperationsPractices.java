72100fd6b7c71ddc083712055ae02c35fdc1669f
Kasun Buddhi Ranaweera <kasunbuddhi@gmail.com>
2024-11-17 23:02:46 +0530

    Streams Intermediate Operators

--- src/com/one/stream/IntermediateOperationsPractices.java
+++ src/com/one/stream/IntermediateOperationsPractices.java
@@ -9,5 +9,11 @@ public class IntermediateOperationsPractices {
         Stream.of("galway", "mayo", "roscommon")
                 .filter(countryName -> countryName.length() > 5)
                 .forEach(System.out::println);
+
+        //distinct
+        Stream.of("eagle", "eagle", "EAGLE")
+                .peek(s -> System.out.println("1."+s))
+                .distinct()
+                .forEach(s -> System.out.println("2."+s));
     }
 }
